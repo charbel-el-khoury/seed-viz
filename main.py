34a14373b12f9f08a0e41cbff74155ed1be8b1a2
celkhoury <celkhoury@ethz.ch>
2024-06-19 13:36:44 +0200

    gee

--- main.py
+++ main.py
@@ -55,10 +55,9 @@ Map.addLayer(im_with_all_bands.select('6_Seed Index'), vis_params, name='Seed In
 st.title('SEED Index Visualizer')
 col1, col2 = st.columns([4, 2])
 with col1:
-    with st.form(key='my_form'):
-        
-        map_data = st_folium(Map, width=1200, height=700)
+    with st.form(key='aa'):
         st.form_submit_button(label='Submit')
+        map_data = st_folium(Map, width=1200, height=700)
 
     st.markdown("for complaints please contact: [Robert McElderry](https://picsum.photos/2000)")
     st.image('figs/ETH+CL Logo_white+yellow.png', width= 300)
